7f3fb5dd3fdd933a3c9849f7e4a92f112954afb0
Willtubbs01 <copa.nathan@gmail.com>
2026-01-08 21:46:05 -0600

    added the player input and the enter button

--- pages/Dino_Pick.js
+++ pages/Dino_Pick.js
@@ -4,7 +4,15 @@ let cluesRevealed = false;
 const cluesContainer = document.querySelector(".hidden-clues");
 const clueButton = document.querySelectorAll(".clue-btn");
 
+const input = document.getElementById("user-input");
+const submitBtn = document.getElementById("submit-btn");
+
 function onPlayerGuess() {
+
+    const guess = input.value.trim();
+    if(!guess) return;
+
+    console.log(guess)
     guessCount++;
 
     if(!cluesRevealed && guessCount >= 1){
@@ -18,4 +26,13 @@ function onPlayerGuess() {
             a.classList.add("unlocked");
         }
     });
+
+    input.value = "";
 }
+
+submitBtn.addEventListener("click", onPlayerGuess);
+input.addEventListener("keydown", e => {
+    if(e.key == "Enter"){
+        onPlayerGuess();
+    }
+})
