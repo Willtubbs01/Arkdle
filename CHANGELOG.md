5c0ec97826fdfd0bcba2949b4af45e5555fba0dc
Willtubbs01 <copa.nathan@gmail.com>
2026-01-08 23:31:59 -0600

    Created the search bar and made it work with a test of 10 dinos

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,7 @@ Patch 0.0.1
 - Added visuals and clues to the Guess of the day
 - Added the JavaScript for the dino guess page so that clue buttons are shown when a guess is answered
 - Added the player input and the enter button that will show the clues when pressed with a guess input
-- 
+- Clues are now in the middle of the Guess of the day!
 
 Patch 0.0
 - Home Page mostly finished all buttons work
--- pages/Dino_Pick.js
+++ pages/Dino_Pick.js
@@ -1,6 +1,10 @@
 let guessCount = 0;
 let cluesRevealed = false;
 
+let dinos = [];
+let dinoNames = [];
+
+
 const cluesContainer = document.querySelector(".hidden-clues");
 const clueButton = document.querySelectorAll(".clue-btn");
 
@@ -36,3 +40,45 @@ input.addEventListener("keydown", e => {
         onPlayerGuess();
     }
 })
+
+//Dino Json
+function updateDetails(query){
+    const list = document.getElementById("dino-list");
+    list.innerHTML = "";
+
+    const q = query.trim().toLowerCase();
+    if(!q) return;
+
+    const matches = dinoNames
+    .filter(name => name.toLowerCase().includes(q))
+    .slice(0,20); // 20 names max
+
+    for(const name of matches){
+        const opt = document.createElement("option");
+        opt.value = name;
+        list.appendChild(opt);
+    }
+}
+
+document.addEventListener("DOMContentLoaded",async () => {
+    await loadDinos();
+
+    const input = document.getElementById("user-input");
+    input.addEventListener("input", () => updateDetails(input.value));
+    input.addEventListener("focus", () => updateDetails(input.value))
+});
+
+
+//Fetch Commmands
+async function loadDinos() {
+    const res = await fetch("../json_packages/dinos.json");
+    if(!res.ok) throw new Error("Failed to get json");
+    const data = await res.json();
+
+    dinos = Array.isArray(data.dinos) ? data.dinos : [];
+    dinoNames = dinos
+    .map(d => d?.name)
+    .filter(Boolean)
+    .sort((a,b) => a.localeCompare(b));
+}
+
